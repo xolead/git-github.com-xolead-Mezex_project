c45c49e16949ebe2b87a36da4712db094fb52786
poltorashka22 <72139051+poltorashka22@users.noreply.github.com>
2024-03-27 23:25:51 +0500

    Add files via upload

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)

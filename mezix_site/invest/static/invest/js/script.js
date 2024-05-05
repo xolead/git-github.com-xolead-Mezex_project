87962614c553defdb471a70c2b3333432e3700f2
xolead <imcsasa@mail.ru>
2024-05-05 06:30:54 +0500

    Buttons-predlozit+otkaz_ozivleni

--- mezix_site/invest/static/invest/js/script.js
+++ mezix_site/invest/static/invest/js/script.js
@@ -14,3 +14,33 @@ function opense() {
         document.getElementById("strelka-vlevo").style.rotate = "180deg";
     }
 }
+
+window.pre_elem = "0"
+
+function get_id_delete(event){
+    window.elem = event.target.id; 
+    document.getElementById(elem).style.cursor = "default";
+
+    if  (document.getElementById("popup").style.zIndex=="-1")
+    {
+        document.getElementById("button-block-applications").style.marginRight = "365px";
+        document.getElementById("popup").style.zIndex = "1";
+        document.getElementById("strelka-vlevo").style.rotate = "180deg";
+    }
+
+    if (pre_elem != elem)
+    {   
+        if (pre_elem != "0"){
+            document.getElementById(pre_elem).style.cursor = "pointer";
+            document.getElementById(elem).style.cursor = "default";
+        }
+    }
+
+    pre_elem = elem;
+    return elem
+}
+
+function deletes() {
+    document.getElementById(elem).remove();
+    pre_elem = "0"
+}
